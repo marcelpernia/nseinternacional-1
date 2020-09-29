d0f32f351da4462df817f99241eece1c920d1987
marcelm <mpernia@riservi.com>
2020-09-29 12:43:14 -0500

    se ha reemplazado el servicio de envio de email por uno mas rapido

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -1,50 +1,62 @@
 <template>
-	<div class="section">
-		<div class="container">
-			<h2 class="title is-4 has-text-centered">Contacto</h2>
+	<div>
+		<div class="section">
+			<div class="container pb-5">
+				<h2 class="title is-4 has-text-centered">Contacto</h2>
 
-			<div class="columns is-centered">
-				<div class="column is-half">
-					<div v-if="feedback" class="notification is-success is-light mb-2">
-						<button class="delete" @click="feedback = false"></button>
-						<strong>Hemos recibido tu mensaje.</strong> Gracias por escribirnos!
-					</div>
-					<div class="box">
-						<div class="field">
-							<input 
-								type="text" 
-								class="input is-fullwidth" 
-								placeholder="Nombre" 
-								v-model="nombre"
-								required>	
+				<div class="columns is-centered">
+					<div class="column is-half">
+						<div v-if="feedback" :class="" class="notification is-success is-light mb-2">
+							<button class="delete" @click="feedback = false"></button>
+							<strong>Hemos recibido tu mensaje.</strong> Gracias por escribirnos!
 						</div>
-						<div class="field">
-							<input 
-								type="email" 
-								class="input is-fullwidth" 
-								placeholder="Correo electrónico"
-								v-model="email"
-								required>	
-						</div>
-						<div class="field">
-							<textarea 
-								class="textarea" 
-								placeholder="Mensaje"
-								v-model="mensaje"></textarea>
-						</div>
-						<div class="field">
-							<button class="button is-link is-medium is-fullwidth" :class="{'is-loading': sending}" @click="enviar">
-								<span>Enviar mensaje</span>
-								<span class="icon"><i class="fa fa-paper-plane"></i></span>
-							</button>
+						<div class="box">
+							<form @submit.prevent="sendEmail">
+								<div class="field">
+									<span class="label">Nombre</span>
+									<input 
+										type="text" 
+										class="input is-fullwidth" 
+										placeholder="Nombre" 
+										name="nombre"
+										v-model="nombre"
+										required>	
+								</div>
+								<div class="field">
+									<span class="label">Correo electrónico</span>
+									<input 
+										type="email" 
+										class="input is-fullwidth" 
+										placeholder="Correo electrónico"
+										name="email"
+										v-model="email"
+										required>	
+								</div>
+								<div class="field">
+									<span class="label">Mensaje</span>
+									<textarea 
+										class="textarea" 
+										placeholder="Mensaje"
+										name="mensaje"
+										v-model="mensaje"></textarea>
+								</div>
+								<div class="field">
+									<button type="submit" :class="{'is-loading': sending}" class="button is-link is-medium is-fullwidth">
+										<span>Enviar mensaje</span>
+										<span class="icon"><i class="fa fa-paper-plane"></i></span>
+									</button>
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>	
+		</div>	
+		<div class="section"></div>
+	</div>
 </template>
 <script>
+	import emailjs from 'emailjs-com';
 	export default {
 		head() {
 	    return {
@@ -64,43 +76,32 @@
 	  	}
 	  },
 		methods: {
-			enviar() {
+			sendEmail(e) {
 				this.sending = true
-				Email.send({
-			    Host : "nseinternacional.org",
-			    Username : "info@nseinternacional.org",
-			    Password : "CXR!6k8Raj9H",
-			    To : 'marcelpm1987@gmail.com',
-			    From : "info@nseinternacional.org",
-			    Subject : `Mensaje de ${this.nombre}`,
-			    Body : `
-			    	Nombre: ${this.nombre}<br>
-			    	Email: ${this.email}<br>
-			    	Mensaje: ${this.mensaje}<br>.<br>.<br>.<br>
-			    	<span style="font-size: 12px;color: #8C8E8E;">Mensaje enviado desde nseinternacional.org</span>
-			    `
-				})
-				.then(message => {
-					// alert(message)
-					this.feedback = true
-				})
-				.catch(error => {
-					console.log(error)
-				})
-				.finally(() => {
-					this.sending = false
-					this.nombre = ''
-		  		this.email = ''
-		  		this.mensaje = ''
-				})
-			}
+
+	      emailjs.sendForm('service_a4ci666', 'template_c4mp1xf', e.target, 'user_xHECOOvKNOTcwdQtVTmCB')
+	        .then((result) => {
+	          console.log('SUCCESS!', result.status, result.text);
+
+	        	this.feedback = true
+	        })
+	        .catch(error => {
+	        	console.log('FAILED', error)
+	        })
+	        .finally(() => {
+						this.sending = false
+						this.nombre = ''
+			  		this.email = ''
+			  		this.mensaje = ''
+					})
+	    }
 		}
 	}
 </script>
 <style lang="scss" scoped>
 	.section {
 		height: 100%;
-		/*background-color: #f8f8f8 !important;*/
-		/*box-shadow: inset 0 3000px 0 #f8f8f8;*/
+		background-color: #f8f8f8 !important;
+		box-shadow: 0 3000px 0 #f8f8f8;
 	}
 </style>
